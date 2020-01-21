51eebf9b1af2c82f51732d5ecc3dadaf3ef09843
ducksecops <daniel@ducksecops.uk>
2020-01-21 13:19:11 +0000

    Initial commit Microsoft Web Platform 5.1.51515.0

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,25 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url        = 'https://download.microsoft.com/download/8/4/9/849DBCF2-DFD9-49F5-9A19-9AEE5B29341A/WebPlatformInstaller_x86_en-US.msi'
+$url64      = 'https://download.microsoft.com/download/8/4/9/849DBCF2-DFD9-49F5-9A19-9AEE5B29341A/WebPlatformInstaller_x64_en-US.msi'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI' #only one of these: exe, msi, msu
+  url           = $url
+  url64bit      = $url64
+  
+  softwareName  = 'Microsoft Web Platform Installer 5.1' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+
+  checksum      = '62E871352B413BC976D8E9402697C684B26807EB863B14D2F54677BDB969DD14'
+  checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
+  checksum64    = '13F4CBB9FA554F77F2AB407BAA40501FC6C131F3077D48AAB839CE63ED8A90E2'
+  checksumType64= 'sha256' #default is checksumType
+
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
